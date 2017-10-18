36e2a724e38942f9bdc442dc3c461bc65028f78a
Gyles Batara and Marcus Parmentier <gyles@>
2017-10-18 09:43:32 -0700

    Complete all logic tests

--- RPS.Tests/ModelTests/RPS.Tests.cs
+++ RPS.Tests/ModelTests/RPS.Tests.cs
@@ -22,5 +22,61 @@ namespace RPS.Tests
       //Assert
       Assert.AreEqual(expected, result);
     }
+
+    [TestMethod]
+    public void ComparePlayers_CompareValues2_True()
+    {
+      //Arrange
+      RPSchecker newValue = new RPSchecker();
+
+      //Action
+      var result = newValue.Game(1, 3);
+      var expected = "Player 1 wins!";
+
+      //Assert
+      Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void ComparePlayers_CompareValues3_True()
+    {
+      //Arrange
+      RPSchecker newValue = new RPSchecker();
+
+      //Action
+      var result = newValue.Game(2, 1);
+      var expected = "Player 1 wins!";
+
+      //Assert
+      Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void ComparePlayers_CompareValues4_True()
+    {
+      //Arrange
+      RPSchecker newValue = new RPSchecker();
+
+      //Action
+      var result = newValue.Game(3, 2);
+      var expected = "Player 1 wins!";
+
+      //Assert
+      Assert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    public void ComparePlayers_CompareValues5_True()
+    {
+      //Arrange
+      RPSchecker newValue = new RPSchecker();
+
+      //Action
+      var result = newValue.Game(2, 3);
+      var expected = "Player 2 wins!";
+
+      //Assert
+      Assert.AreEqual(expected, result);
+    }
   }
 }
--- RPS/Models/RPS.cs
+++ RPS/Models/RPS.cs
@@ -11,6 +11,18 @@ namespace RPS.Models
       {
         return "IT'S A DRAW!";
       }
+      else if(Player1 == 1 && Player2 == 3)
+      {
+        return "Player 1 wins!";
+      }
+      else if(Player1 == 2 && Player2 == 1)
+      {
+        return "Player 1 wins!";
+      }
+      else if(Player1 == 3 && Player2 == 2)
+      {
+        return "Player 1 wins!";
+      }
       else
       {
         return "Player 2 wins!";
